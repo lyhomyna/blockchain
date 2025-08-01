03047b0e7e82db6defcd92029029acf125d30e9e
maksym <maximligomina011@gmail.com>
2025-08-01 17:27:04 +0300

    Proof-Of-Work#1

--- proof.go
+++ proof.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"math"
 	"math/big"
 )
 
+// difficulty of mining
 // hash must start with { targetBits } zeroes
 const targetBits = 24
+const maxNonce = math.MaxInt64
 
 type ProofOfWork struct {
     block *Block
@@ -20,3 +26,38 @@ func NewProofOfWork(b *Block) *ProofOfWork {
 
     return pow
 }
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+    var hashInt big.Int
+    var hash [32]byte
+    nonce := 0
+
+    for nonce < maxNonce {
+	data := pow.prepareData(nonce)
+	hash = sha256.Sum256(data)
+	fmt.Printf("\r%x", hash)
+	hashInt.SetBytes(hash[:])
+
+	if hashInt.Cmp(pow.target) == -1 {
+	    break
+	} else {
+	    nonce++
+	}
+    }
+    fmt.Print("\n\n")
+
+    return nonce, hash[:]
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+    data := bytes.Join([][]byte{
+	pow.block.Data,
+	pow.block.PrevBlockHash,
+	IntToHex(pow.block.Timestamp),
+	IntToHex(int64(nonce)),
+	IntToHex(int64(targetBits)),
+    }, []byte{})
+
+    return data
+}
+
---
+++ utils.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"log"
+)
+
+func IntToHex(num int64) []byte {
+    buff := new(bytes.Buffer)
+
+    if err := binary.Write(buff, binary.BigEndian, num); err != nil {
+	log.Panic(err)
+    }
+    
+    return buff.Bytes()
+}
